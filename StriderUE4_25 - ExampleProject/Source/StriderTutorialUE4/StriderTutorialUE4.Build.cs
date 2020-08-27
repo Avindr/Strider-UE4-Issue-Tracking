08849e8db474b0a5643c65c6cfd019b784ae6c9f
Kenneth Claassen <claassen.ken@gmail.com>
2020-08-27 10:21:13 +1000

    Fixes for initial plugin submission

--- StriderUE4_25 - ExampleProject/Source/StriderTutorialUE4/StriderTutorialUE4.Build.cs
+++ StriderUE4_25 - ExampleProject/Source/StriderTutorialUE4/StriderTutorialUE4.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2020 Kenneth Claassen, All Rights Reserved.
 
 using UnrealBuildTool;
 
--- StriderUE4_25 - ExampleProject/Source/StriderTutorialUE4/StriderTutorialUE4.cpp
+++ StriderUE4_25 - ExampleProject/Source/StriderTutorialUE4/StriderTutorialUE4.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2020 Kenneth Claassen, All Rights Reserved.
 
 #include "StriderTutorialUE4.h"
 #include "Modules/ModuleManager.h"
